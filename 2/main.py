6716eef70e5bc7ce0ce6d0981084e22daba7bf64
Karel Jakubec <kaj@revolgy.com>
2018-12-01 22:48:30 +0100

    Add 2dn assignment

---
+++ 2/main.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+import os
+
+
+def main():
+    sum = 0
+    
+    seen = set()
+    seen.add(sum)
+
+    all = []
+    with open(os.path.dirname(__file__) + "/data") as f:
+        for line in f:
+            all.append(int(line))
+
+    i = 0
+    while(True):
+        sum += all[i]
+        if sum in seen:
+            break
+        else:
+            seen.add(sum)
+        
+        if i >= len(all) - 1:
+            i = 0
+            print("Iteration")
+        else:
+            i += 1
+
+    print(sum)
+
+if __name__ == "__main__":
+    main()
