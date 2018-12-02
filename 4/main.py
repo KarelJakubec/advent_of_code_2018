503e358f21437ea605dfea954810560f547d1a70
Karel Jakubec <kaj@revolgy.com>
2018-12-02 23:40:22 +0100

    Add 4th puzzle

---
+++ 4/main.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+import os
+
+def main():
+    ar = []
+
+    with open(os.path.dirname(__file__) + "/data") as f:
+        for line in f:
+            ar.append(line.strip('\n'))
+    
+    for i1 in ar:
+        for i2 in ar:
+            diff = 0
+            for i in range(0, len(i1)):
+                if i1[i] != i2[i]:
+                    diff += 1
+
+                # if diff > 1:
+                #     break
+            
+            if diff == 1:
+                out = ""
+                for i in range(0, len(i1)):
+                    if i1[i] == i2[i]:
+                        out += i1[i]
+                
+                print(out)
+                return 
+
+
+
+
+if __name__ == "__main__":
+    main()
